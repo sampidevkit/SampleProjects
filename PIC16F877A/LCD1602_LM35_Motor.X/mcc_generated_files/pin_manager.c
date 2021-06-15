5c11e5e5a17ef73708c49007b7768844ce2796a2
sampidevkit <sampidevkit@gmail.com>
2021-06-15 21:56:45 +0700

    PIC16F877A: LCD1602 - LM35 - DC motor

---
+++ PIC16F877A/LCD1602_LM35_Motor.X/mcc_generated_files/pin_manager.c
@@ -0,0 +1,16 @@
+#include "pin_manager.h"
+
+void PIN_MANAGER_Initialize(void)
+{
+    PORTA=0b11111111;
+    PORTC=0b11111010;
+    PORTD=0b11000000;
+    
+    TRISA=0b11111111;
+    TRISC=0b11111010;
+    TRISD=0b11000000;
+}
+
+void PIN_MANAGER_IOC(void)
+{
+}
